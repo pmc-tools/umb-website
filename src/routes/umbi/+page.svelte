3a81beb12ff7fd9fea5913a48dca42d7e3496f77
Josh Jeppson <joshua.jeppson17@gmail.com>
2026-01-20 16:16:19 -0700

    added page for umb

--- src/routes/umbi/+page.svelte
+++ src/routes/umbi/+page.svelte
@@ -1,3 +1,34 @@
 <div class=content>
-	The reference implementation
+	<h1>UMBI: The Reference UMB Implementation</h1>
+	UMBI is a reference implementation for the UMB standard written in Python. It is open source, freely licensed under the MIT license, and is available <a href=https://github.com/randriu/umbi>on GitHub</a>.
+	<h2>Installation</h2>
+	UMBI is available on either <a href=https://github.com/randriu/umbi>GitHub</a> or as a <code>pip</code> package on <a href=https://pypi.org/project/umbi>PyPi</a>. In this, we will show installation via <code>pip</code>.
+
+	<h3>Step 1:</h3> First, ensure that <code>pip</code> is installed via the <code>ensure-pip</code> module. Optionally, then create a virtual environment if you do not want UMBI installed globally or at a user-level. This will create a virtual environment in the current working directory in a hidden directory called <code>.venv</code>.
+	<div class=code>
+<span class=code-comment># Note: on Debian and Ubuntu, the executable name is `python3`</span><br>
+python -m ensure-pip<br>
+<span class=code-comment># The following step is optional</span><br>
+python -m venv .venv
+source .venv/bin/activate</div>
+	<h3>Step 2:</h3>
+	Finally, we install UMBI.
+	<div class=code>
+python3 -m pip install umbi<br>
+<span class=code-comment># If you are using UMBI in a project, we recommend you create a requirements.txt file.</span><br>
+echo <span class=code-string>'umbi'</span> &nbsp;>> requirements.txt
+	</div>
+	<h3>Step 3:</h3>
+	Now you probably want to <i>use</i> UMBI. There are two ways to do this:
+	<ol>
+		<li>Directly via the CLI</li>
+		<li>By the Python API</li>
+	</ol>
+	To use UMBI directly, we provide a CLI via the <code>umbi</code> executable.
+<div class=code>
+umbi --import-umb /path/to/input.umb<br>
+umbi --import-umb /path/to/input.umb --export-umb /path/to/output.umb<br>
+umbi --import-umb /path/to/input.umb --export-umb /path/to/output.umb --log-level=DEBUG
+</div>
+We will add documentation for UMBI's Python API later.
 </div>
