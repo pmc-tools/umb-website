0495f727e342e6879f2951830714596001185491
Josh Jeppson <joshua.jeppson17@gmail.com>
2026-01-27 15:28:50 -0700

    svelte links are different from html links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <svelte:head>
-	<link rel=stylesheet href="{base}/styles.css"></link>
+	<link rel=stylesheet href="{base}/styles.css" />
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
